70d8ffd1f4d865e9bff304853eae3cbaef21c6da
EmyleSilva <emylesnt.silva@gmail.com>
2025-02-02 16:01:34 -0300

    Implementação de temporização periódica

---
+++ Embarcatech-Temporizador_T1.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/timer.h"
+
+#define led_r 13
+#define led_g 11
+#define led_b 12
+
+const uint8_t pins[] = {led_r, led_g, led_b};
+uint8_t counter = 0;
+
+int leds_states[3][3] = {
+    {1, 0, 0},
+    {0, 1, 0},
+    {0, 0, 1}
+};
+
+void init_leds()
+{
+    gpio_init(led_r);
+    gpio_set_dir(led_r, GPIO_OUT);
+
+    gpio_init(led_g);
+    gpio_set_dir(led_g, GPIO_OUT);
+    
+    gpio_init(led_b);
+    gpio_set_dir(led_b, GPIO_OUT);
+}
+
+void turn_leds_on()
+{
+    for (int i = 0; i < 3; i++)
+        gpio_put(pins[i], leds_states[counter][i]);
+}
+
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+
+    turn_leds_on();
+
+    counter = ++counter > 2 ? 0 : counter;
+    printf("Passou %d segundo(s)\n", counter);
+    return true;
+}
+
+int main()
+{
+    stdio_init_all();
+    init_leds();
+
+    struct repeating_timer timer;
+
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
+
+    while (true) {
+        sleep_ms(1000);
+        printf("Passou 1 segundo\n");
+    }
+}
